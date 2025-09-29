869a84fb6d07cc04ac40e6aac13a48b2446f0373
Nokayto <luismaturanot@gmail.com>
2025-09-29 11:33:22 -0400

    Implementacion de lan forma basica de la primera vista del programa

--- pizzeria.py
+++ pizzeria.py
@@ -8,9 +8,29 @@ conexion = mysql.connector.connect(
     database="local"       
 )
 
-if conexion.is_connected():
-    print("¡Conexión exitosa con MySQL!")
-    cursor = conexion.cursor()
-    cursor.execute("SELECT DATABASE();")
-    resultado = cursor.fetchone()
-    print("Base de datos conectada:", resultado)
+#VENTANA PRINCIPAL
+root = tk.Tk()
+root.title("PIZZERIA")
+root.state('zoomed')
+
+#CAJERO-PIZZERO-ADMINISTRACION
+
+
+boton_cajero = tk.Button(root, text="CAJERO",font=("Times New Roman",100))
+boton_cajero.pack(pady=20)
+boton_cajero.place(x=200,y=200)
+
+boton_admin = tk.Button(root, text="ADMIN",font=("Times New Roman",100))
+boton_admin.pack(pady=20)
+boton_admin.place(x=600,y=500)
+
+boton_pizzero = tk.Button(root, text="PIZZERO",font=("Times New Roman",100))
+boton_pizzero.pack(pady=20)
+boton_pizzero.place(x=1000,y=200)
+
+titulo = tk.Label( text="BIENBENIDO",font=("Times New Roman",50))
+titulo.pack(pady=20,padx=20)
+titulo.place(x=750,y=10)
+
+#ejecutar ventana
+root.mainloop()
